ea8dd3cefb702625ca0a31982628a43a6f2a10a5
alisa <alisa.tatosova110@gmail.com>
2024-08-04 00:29:32 +0400

    Adding copy, move constructors and assignments, constructors from string and int, operator[] for const and non-const objects, to_string, to_ulong

--- bitset.cpp
+++ bitset.cpp
@@ -1,37 +1,118 @@
 #include "bitset.h"
 
 template <std::size_t N>
-Bitset<N>::Bitset() {
+void Bitset<N>::initialize() {
     int length = N / BYTE + (N % BYTE != 0);
     unsigned char* bitset = new unsigned char[length]();
     this -> bitset = bitset; 
+    this -> length = length;
 }
 
 template <std::size_t N>
-void Bitset<N>::set(std::size_t index)  {
+Bitset<N>::Bitset() {
+    initialize();
+}
+
+template <std::size_t N>
+Bitset<N>::Bitset(const std::string& bits) {
+    initialize();
+    int start = 0;
+    if (bits.size() > N) {
+        start = bits.size() - N;
+    }
+    for (int i = 0; i < bits.size() && i < N; ++i) {
+        if (bits[bits.size() - 1 - i - start] == '1') {
+            set(i);
+        } else if (bits[bits.size() - 1 - i - start] != '0') {
+            throw std::invalid_argument("Invalid character in string. Only '0' and '1' are allowed to be given in string as bit.");
+        } 
+    }
+}
+template <std::size_t N>
+Bitset<N>::Bitset(unsigned int number) {
+    initialize(); 
+    for (int i = 0; i < N; ++i) {
+        if (number == 0) {
+            break;
+        }
+
+        if (number & 1) { 
+            set(i);
+        }
+        number >>= 1;
+    }
+}
+
+template <std::size_t N>
+Bitset<N>::Bitset(const Bitset& other) : length{other.length} {
+    bitset = new unsigned char[other.length]();
+    for (int i = 0; i < other.length; ++i) {
+        bitset[i] = other.bitset[i];
+    }
+}
+
+template <std::size_t N>
+Bitset<N>::Bitset(Bitset&& other) noexcept
+    : length{other.length}, bitset{other.bitset} {
+    other.bitset = nullptr;
+    other.length = 0;
+}
+
+template <std::size_t N>
+Bitset<N>& Bitset<N>::operator=(const Bitset<N>& other) {
+    if (this == &other) {
+        return *this;
+    }
+    delete[] bitset;
+    length = other.length;
+    bitset = new unsigned char[length]();
+    for (int i = 0; i < length; ++i) {
+        bitset[i] = other.bitset[i];
+    }
+    return *this;
+}
+
+template <std::size_t N>
+Bitset<N>& Bitset<N>::operator=(Bitset<N>&& other) noexcept {
+    if (this != &other) {
+        delete[] bitset;
+        bitset = other.bitset;
+        length = other.length;
+        other.bitset = nullptr;
+        other.length = 0;
+    }
+    return *this;
+}
+
+template <std::size_t N>
+std::size_t Bitset<N>::get_mask(std::size_t index) const {
+    int pos = index % BYTE;
+    return 1 << pos;
+}
+
+template <std::size_t N>
+void Bitset<N>::set(std::size_t index, bool bit_status)  {
     check_out_of_range(index);
     int bitset_index = index / BYTE;
-    int pos = index % BYTE;
-    int mask = 1 << pos;
-    bitset[bitset_index] |= mask;
+    if (bit_status) {
+        bitset[bitset_index] |= get_mask(index);
+    } else {
+        reset(index);
+    }
 }
 
 template <std::size_t N>
 void Bitset<N>::reset(std::size_t index) {
     check_out_of_range(index);
     int bitset_index = index / BYTE;
-    int pos = index % BYTE;
-    int mask = 1 << pos;
-    bitset[bitset_index] &= ~mask;
+    bitset[bitset_index] &= ~get_mask(index);
 }
 
 template <std::size_t N>
 void Bitset<N>::flip(std::size_t index) {
     check_out_of_range(index);
     int bitset_index = index / BYTE;
-    int pos = index % BYTE;
-    int mask = 1 << pos;
-    bitset[bitset_index] ^= mask;
+    bitset[bitset_index] ^= get_mask(index);
 }
 
 template <std::size_t N>
@@ -79,16 +160,52 @@ template <std::size_t N>
 bool Bitset<N>::test(std::size_t i) const {
     check_out_of_range(i);
     int bitset_index = i / BYTE;
-    int pos = i % BYTE;
-    int bit_status = (bitset[bitset_index] >> pos) & 1;  	
-    return bit_status;
+    int pos = i % BYTE;	
+    return (bitset[bitset_index] >> pos) & 1;
+}
+
+template <std::size_t N>
+BitManipulator<N> Bitset<N>::operator[](std::size_t index) {
+    check_out_of_range(index);
+    return BitManipulator<N>(*this, index);
+}
+
+template <std::size_t N>
+bool Bitset<N>::operator[](std::size_t index) const {
+    return test(index);
 }
 
 template <std::size_t N>
 void Bitset<N>::check_out_of_range(std::size_t pos) const {
-    if (pos < 0 || pos >= N) {
+    if (pos >= N) {
         throw std::out_of_range("Index is out of range");
     }
+}
+
+template <std::size_t N>
+unsigned long Bitset<N>::to_ulong() const{
+    unsigned long result = 0;
+    for (int i = 0; i < N; ++i) {
+        if (i > sizeof(unsigned long) * 8 && test(i)) {
+            throw std::overflow_error("Bitset size exceeds the maximum number of bits in an unsigned long.");
+        }
+
+        if (test(i)) {
+            result |= (1 << i);
+        }
+    }
+    return result;
+}
+
+template <std::size_t N>
+std::string Bitset<N>::to_string() const {
+    std::string str(N, '0');
+    for (std::size_t i = 0; i < N; ++i) {
+        if (test(i)) {
+            str[N - 1 - i] = '1';
+        }
+    }
+    return str;
 }
 
 template <std::size_t N>
--- bitset.h
+++ bitset.h
@@ -2,12 +2,20 @@
 #define BITSET_H
 #include <iostream>
 #include <cstddef>
+#include "bit_manipulator.h"
 
 template <std::size_t N>
 class Bitset {
 public:
-    explicit Bitset();
-    void set(std::size_t index) ;
+    Bitset();
+    explicit Bitset(const std::string& bits);
+    explicit Bitset(unsigned int number);
+    Bitset(const Bitset& other);
+    Bitset& operator=(const Bitset& other);
+    Bitset(Bitset&& other) noexcept;
+    Bitset& operator=(Bitset&& other) noexcept;
+    void initialize();
+    void set(std::size_t index, bool value = true);
     void reset(std::size_t index);
     void flip(std::size_t index);
     int count() const;
@@ -15,16 +23,25 @@ public:
     bool none() const;
     bool all() const;
     bool test(std::size_t index) const;
-    void check_out_of_range(std::size_t pos) const;
     int size() const;
+    unsigned long to_ulong() const;
+    std::string to_string() const;
+    
+    BitManipulator<N> operator[](std::size_t index);
+    bool operator[](std::size_t index) const;
 
-    template <std::size_t M>
+    template <std::size_t M> 
     friend std::ostream& operator<<(std::ostream& os, const Bitset<M>& set);
 
     ~Bitset();
 
 private:
+    void check_out_of_range(std::size_t pos) const;
+    std::size_t get_mask(std::size_t index) const;
+
+private:
     unsigned char* bitset;
+    std::size_t length;
     static const int BYTE = 8;
 };
 
